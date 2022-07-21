48583e7905097e4862548dac365441400347b7e8
Derek J. Curtis <derek.curtis@innotescus.io>
2022-07-21 10:16:52 -0400

    Made example more generic

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import hmac
 from http import HTTPStatus
 
 from flask import Flask, request
-from innotescus import client_factory
 from werkzeug.exceptions import Forbidden
 
 
@@ -12,10 +11,8 @@ app = Flask(__name__)
 YOUR_SECRET_KEY = b'YOUR_SECRET_KEY'
 
 
-@app.route('/job', methods=['POST'])
-def export_ready():
-    """ Example handler for the innotescus "EXPORT READY" webhook.
-    """
+@app.route('/your-webhook-callback', methods=['POST'])
+def your_webhook_callback():
 
     # construct expected HMAC signature
     base_string = ':'.join([
@@ -25,25 +22,12 @@ def export_ready():
     ])
     expected = hmac.new(YOUR_SECRET_KEY, base_string.encode(), hashlib.sha256).hexdigest()
 
-    # verify signature or fail request
+    # verify signature or fail request 
     actual = request.headers['X-Innotescus-Signature']
     if not hmac.compare_digest(expected, actual):
         raise Forbidden('HMAC Signature Validation Failed')
 
-    # read the job_id from the request payload
-    job_id = request.json['job_id']
-    job_type = request.json['type']
-    job_status = request.json['status']
-
-    if job_type == 'export' and job_status == 'ready':
-        # use the innotescus client to download the export
-        # WARNING: your download MUST be completed within 5
-        # minutes or Innotescus will time out.
-        c = client_factory()
-        c.download_export(
-            job_id=job_id,
-            download_path='/some/path'
-        )
+    # TODO: place whatever code you like here to handle the webhook
 
     # return an HTTP 200 response -- the response body and type DO NOT matter
     # if a 200 response is not received, innotescus will try to call your
